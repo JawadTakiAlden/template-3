dee83a8f6a25aca89c6a862a7463b9a84564e7e8
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2022-04-14 03:51:41 -0700

    initial commit

---
+++ js/master.js
@@ -0,0 +1,21 @@
+// let section = document.querySelector(".stats")
+// let nums = document.querySelectorAll(".stats .box .number")
+// let started = false
+// function IncrementNumber(num){
+//     let goal = num.dataset.goal;
+//     let count = setInterval(() => {
+//         num.textContent++;
+//         if(num.textContent == goal){
+//             clearInterval(count)
+//         }
+//     }, 2000/progres)
+// }
+
+// window.onscroll = function(){
+//     if(window.scrollY >= section.offsetTop){
+//         if(!started){
+//             nums.forEach((num) => IncrementNumber(num))
+//         }
+//         started = true
+//     }
+// }
